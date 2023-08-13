75ff29e1f8903d0b31c117dce56d6ebc04dc8946
thisislauralima <thisislauralima@hotmail.com>
2023-08-13 19:21:15 -0300

    add SqlServerDbContext

---
+++ GamersEcommerce.Infrastructure.Oracle/SqlServerDbContext.cs
@@ -0,0 +1,37 @@
+﻿using GamersEcommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GamersEcommerce.Infrastructure.Oracle
+{
+    public class SqlServerDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        
+        public SqlServerDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductSaleHistory> ProductsSaleHistory { get; set; }
+        public DbSet<ProductStockQuantity> ProductsStockQuantity { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+
+            optionsBuilder
+                .EnableDetailedErrors()
+                .UseSqlServer(
+                    connectionString: _configuration.GetConnectionString(nameof(SqlServerDbContext)),
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.EnableRetryOnFailure(
+                            maxRetryCount: 10,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null);
+                    });
+        }
+    }
+}
